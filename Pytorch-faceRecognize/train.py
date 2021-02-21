2aec97127e505dce10da481a45deb2ec0b0db75a
Jaso0n <331831788@qq.com>
2021-02-21 16:12:41 +0800

    version 0.2

--- Pytorch-faceRecognize/train.py
+++ Pytorch-faceRecognize/train.py
@@ -1,7 +1,7 @@
 import os
 import os.path as osp
-
-
+import logging
+import time
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -14,6 +14,23 @@ from config import Config as conf
 import test
 
 print("Import OK")
+def get_logger(filename, verbosity=1, name=None):
+    level_dict = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING}
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(filename)s] %(message)s"
+    )
+    logger = logging.getLogger(name)
+    logger.setLevel(level_dict[verbosity])
+
+    fh = logging.FileHandler(filename, "w")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    return logger
 
 def train(conf):
     dataloader,class_num = load_data(conf, training=True)
@@ -54,8 +71,11 @@ def train(conf):
 
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=conf.lr_step, gamma=0.1)
     os.makedirs(conf.checkpoints, exist_ok = True)
-    best_acc = 0
-    best_th = 0
+    #best_acc = 0
+    #best_th = 0
+    nowtime = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())
+    logger = get_logger("./"+ nowtime +"-train.log")
+    logger.info('start training!')
     net.train()
     for e in range(conf.epoch):
         for batch_idx,data in enumerate(dataloader):
@@ -69,13 +89,14 @@ def train(conf):
             loss.backward()
             optimizer.step()
             if (batch_idx % conf.step_show) == 0:
-                print("Train Epoch:[%d/%d]:interation %d \t \t Loss: %.5f \t \t learning rate: %f" %(e,conf.epoch,batch_idx, loss,scheduler.get_lr()[0]))
-            if (batch_idx % 500) == 0 and batch_idx != 0:#conf.test_step
+                logger.info('Epoch:[{}/{}] \t \t Iteration:[{}] \t \t loss={:.5f} \t \t lr={:.3f}'.format(e ,conf.epoch, batch_idx,loss,scheduler.get_lr()[0]))
+                #print("Train Epoch:[%d/%d]:interation %d \t \t Loss: %.5f \t \t learning rate: %f" %(e,conf.epoch,batch_idx, loss,scheduler.get_lr()[0]))
+            if (batch_idx % 500) == 0 and batch_idx != 0:
                 backbone_path = osp.join(conf.checkpoints, f"{batch_idx}.pth")
                 torch.save(net.state_dict(), backbone_path)
                 accuracy,threshold = test.test(conf,f"./checkpoints/{batch_idx}.pth")
                 print(f"\nLFW Test:Epoch[{e:d}]: Iteration {batch_idx:d} \t \t accuracy: {accuracy:.3f} \t \t threshold: {threshold:.3f}\n")
         scheduler.step()
 
-
-train(conf)
+if __name__ == "__main__":
+    train(conf)
